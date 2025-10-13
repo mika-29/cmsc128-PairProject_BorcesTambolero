59819da3b5a56953d4a2b165ceee28a06c774da2
Michaela F. Borces <mfborces@up.edu.ph>
2025-10-14 00:47:02 +0700

    added new pages and started with backend

---
+++ user_db.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+conn = sqlite3.connect("user.db")   #manages database 
+cursor = conn.cursor()              #leads you send sql commands to the database "middleman" / runs sql statements 
+
+# create table
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+);
+""")
+
+conn.commit()   #makes sure changes are saved 
+conn.close()    #prevents memory leaks 
